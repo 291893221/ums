efd31bd17b0901304b05c3671382d7725d4e33fb
zengly <291893221@qq.com>
2019-05-17 16:31:33 +0800

    DROP TABLE

--- db/drop table/drop table.sql
+++ db/drop table/drop table.sql
@@ -2,12 +2,12 @@ SELECT concat('DROP TABLE IF EXISTS ', table_name, ';')
 FROM information_schema.tables
 WHERE table_schema = 'ums';
 
-DROP TABLE IF EXISTS t_mtm_user_organizition;
+DROP TABLE IF EXISTS t_mtm_user_organization;
 DROP TABLE IF EXISTS t_mtm_user_resource;
 DROP TABLE IF EXISTS t_mtm_user_role;
 DROP TABLE IF EXISTS t_oto_user_ext;
 DROP TABLE IF EXISTS t_ums_log;
-DROP TABLE IF EXISTS t_ums_organizition;
+DROP TABLE IF EXISTS t_ums_organization;
 DROP TABLE IF EXISTS t_ums_resource;
 DROP TABLE IF EXISTS t_ums_role;
 DROP TABLE IF EXISTS t_ums_user;
