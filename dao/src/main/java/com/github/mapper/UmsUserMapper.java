0576682c4d27d2c7eabff13ee5e0304a7336b2f7
zengly <291893221@qq.com>
2019-05-17 14:37:51 +0800

    user

--- dao/src/main/java/com/github/mapper/UmsUserMapper.java
+++ dao/src/main/java/com/github/mapper/UmsUserMapper.java
@@ -4,9 +4,9 @@ import com.github.entity.UmsUser;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
-* Created by Mybatis Generator 2019/05/15
-*/
+ * Created by Mybatis Generator 2019/05/15
+ */
 @Mapper
-public interface UmsUserMapper extends BaseMapper<UmsUser>{
+public interface UmsUserMapper extends BaseMapper<UmsUser> {
 
 }
