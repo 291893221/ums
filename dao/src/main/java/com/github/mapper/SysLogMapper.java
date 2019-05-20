b9b1b292c7152470a68cae93554eb83e175e175e
zengly <291893221@qq.com>
2019-05-20 10:39:36 +0800

    syslog

---
+++ dao/src/main/java/com/github/mapper/SysLogMapper.java
@@ -0,0 +1,12 @@
+package com.github.mapper;
+
+import com.github.entity.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created by Mybatis Generator 2019/05/20
+ */
+@Mapper
+public interface SysLogMapper extends BaseMapper<SysLog> {
+
+}
