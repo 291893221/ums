5e52154423da542bf8694342755ed0bf4e99d749
zengly <291893221@qq.com>
2019-05-21 20:58:34 +0800

    mtm

--- db/create table/many to many/t_mtm_user_resource.sql
+++ db/create table/many to many/t_mtm_user_resource.sql
@@ -4,15 +4,24 @@ IF EXISTS `t_mtm_user_resource`;
 -- 建表
 CREATE TABLE `t_mtm_user_resource` (
   -- 业务字段 start
-	`user_id` BIGINT NOT NULL COMMENT '用户表主键，外键关联 t_ums_user.id',
-	`resource_id` BIGINT  NOT NULL COMMENT '角色表主键，外键关联 t_ums_resource.id',
-	PRIMARY KEY (`user_id`, `resource_id`),
-	KEY `t_mtm_user_resource_fk_u` (`user_id`),
-	KEY `t_mtm_user_resource_fk_r` (`resource_id`),
-	CONSTRAINT `t_mtm_user_resource_fk_u` FOREIGN KEY (`user_id`) REFERENCES `t_ums_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT `t_mtm_user_resource_fk_r` FOREIGN KEY (`resource_id`) REFERENCES `t_ums_resource` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-	-- 业务字段 end
-) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '用户资源关联表';
+  `user_id`     BIGINT NOT NULL
+  COMMENT '用户表主键，外键关联 t_ums_user.id',
+  `resource_id` BIGINT NOT NULL
+  COMMENT '角色表主键，外键关联 t_ums_resource.id',
+  PRIMARY KEY (`user_id`, `resource_id`),
+  KEY `t_mtm_user_resource_fk_u` (`user_id`),
+  KEY `t_mtm_user_resource_fk_r` (`resource_id`),
+  CONSTRAINT `t_mtm_user_resource_fk_u` FOREIGN KEY (`user_id`) REFERENCES `t_ums_user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `t_mtm_user_resource_fk_r` FOREIGN KEY (`resource_id`) REFERENCES `t_ums_resource` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+  -- 业务字段 end
+)
+  ENGINE = INNODB
+  DEFAULT CHARSET = utf8
+  COMMENT = '用户资源关联表';
 -- 初始化数据
 INSERT INTO `t_mtm_user_resource` (`user_id`, `resource_id`)
-  VALUES (1, 1);
+VALUES (1, 1);
--- db/create table/many to many/t_oto_user_ext.sql
+++ db/create table/many to many/t_oto_user_ext.sql
@@ -3,12 +3,16 @@ DROP TABLE
 IF EXISTS `t_oto_user_ext`;
 -- 建表
 CREATE TABLE t_oto_user_ext (
-	-- 业务字段 start
-	`username` VARCHAR (32) NOT NULL UNIQUE COMMENT '登录帐号',
-	`phone` VARCHAR (32) COMMENT '手机号',
-	`email` VARCHAR (32) COMMENT '邮箱',
-	`sex` INT COMMENT '0:女,1:男',
+  -- 业务字段 start
+  `username` VARCHAR(32) NOT NULL UNIQUE
+  COMMENT '登录帐号',
+  `phone`    VARCHAR(32) COMMENT '手机号',
+  `email`    VARCHAR(32) COMMENT '邮箱',
+  `sex`      INT COMMENT '0:女,1:男',
   KEY `t_oto_user_ext_fk` (`username`),
-	CONSTRAINT `t_oto_user_ext_fk` FOREIGN KEY (`username`) REFERENCES `t_ums_user` (`username`)
-	-- 业务字段 end
-) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '用户信息扩展表';
+  CONSTRAINT `t_oto_user_ext_fk` FOREIGN KEY (`username`) REFERENCES `t_ums_user` (`username`)
+  -- 业务字段 end
+)
+  ENGINE = INNODB
+  DEFAULT CHARSET = utf8
+  COMMENT '用户信息扩展表';
